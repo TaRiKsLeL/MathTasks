083b7680c84215aeda217af3f55a247f2eb6b977
Taras <tariksyurts@gmail.com>
2020-04-16 13:43:30 +0300

    hipoten section

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 
 
-var calcBtn = document.getElementById('calc_btn');
+var calcRadBtn = document.querySelector('.circle-section #calc_btn');
+var calcHipotBtn = document.querySelector('.hipoten-section #calc_btn')
 
-calcBtn.addEventListener('click',function(){
+calcRadBtn.addEventListener('click',function(){
 
-    var radius = document.getElementById('radius').value;
-    var result = document.getElementById('result');
+    let radius = document.getElementById('radius').value;
+    let result = document.querySelector('.circle-section #result')
     
     console.clear();
     console.log(radius);
     console.log(typeof radius);
 
     result.innerHTML = "Результат= " + Math.PI*Math.pow(parseInt(radius),2);
+})
+
+calcHipotBtn.addEventListener('click',function(){
+    let kat_A = document.getElementById('kat_A').value;
+    let kat_B = document.getElementById('kat_B').value;
+
+    let result = document.querySelector('.hipoten-section #result')
+
+    result.innerHTML = "Результат= " +Math.sqrt(Math.pow(parseInt(kat_A),2)+Math.pow(parseInt(kat_B),2));
 })
